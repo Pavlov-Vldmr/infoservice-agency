c527c40b507b815308cea7bfab78960b88891a2f
Pavlov-Vldmr <vpavlov95007@gmail.com>
2026-06-19 16:31:18 +0300

    tablet paddings

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,7 +13,7 @@ function Header() {
         <>
             <header className="fixed">
                 <div className="top">
-                    <div className="container top__container">
+                    <div className="container top__container px-8 m_px-0">
                         <div className="top_phone">+7 (495) 123-45-67</div>
                         <div className="top_mail">info@infoservice-охрана.рф</div>
                         <span className="top_guard">Круглосуточная служба безопасности</span>
@@ -21,7 +21,7 @@ function Header() {
                 </div>
 
                 <div className="bottom">
-                    <div className="container bottom__container">
+                    <div className="container bottom__container px-8 m_px-0">
                         <div className="logo">
                             <img src={logo} alt="Логотип Инфосервис" />
                         </div>
--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -7,7 +7,7 @@ function PageTitle(props: { title: string, subTitle: string }) {
 
     return (
         <>
-            <div className="title-component pt-20 pb-12 mt-14 m_mt-4">
+            <div className="title-component pt-20 pb-12 px-8 mt-14 m_mt-4 m_px-0">
                 <div className="container title-component__container m_p-4">
                     <h1 className='m_mb-4'>{props.title}</h1>
                     <p>{props.subTitle}</p>
