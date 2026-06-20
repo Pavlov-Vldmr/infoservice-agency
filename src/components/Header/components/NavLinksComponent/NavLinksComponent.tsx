2f4162d0e1a3c7bb193b920291dae419553f92b4
Pavlov-Vldmr <vpavlov95007@gmail.com>
2026-06-20 12:11:36 +0300

    active link fix

--- src/components/Header/components/NavLinksComponent/NavLinksComponent.tsx
+++ src/components/Header/components/NavLinksComponent/NavLinksComponent.tsx
@@ -8,11 +8,11 @@ function NavLinksComponent() {
     return (
         <>
             <ul className={"nav-links__component"}>
-                <li> <NavLink to="/infoservice-agency">Главная</NavLink></li>
-                <li> <NavLink to="/infoservice-agency/about">О компании</NavLink></li>
-                <li> <NavLink to="/infoservice-agency/services">Услуги</NavLink></li>
-                <li> <NavLink to="/infoservice-agency/objects">Объекты</NavLink></li>
-                <li> <NavLink to="/infoservice-agency/contacts">Контакты</NavLink></li>
+                <li> <NavLink to="/infoservice-agency" end>Главная</NavLink></li>
+                <li> <NavLink to="/infoservice-agency/about" end>О компании</NavLink></li>
+                <li> <NavLink to="/infoservice-agency/services" end>Услуги</NavLink></li>
+                <li> <NavLink to="/infoservice-agency/objects" end>Объекты</NavLink></li>
+                <li> <NavLink to="/infoservice-agency/contacts" end>Контакты</NavLink></li>
             </ul>
         </>
     )
