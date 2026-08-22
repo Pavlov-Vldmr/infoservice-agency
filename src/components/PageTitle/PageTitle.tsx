6a61add17ca3f221133ecf5060343e3e4d782eef
Pavlov-Vldmr <vpavlov95007@gmail.com>
2026-08-22 12:06:42 +0300

    refactor: update PageTitle to use ShinyText component and enhance PhoneCallbackComponent validation

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -1,4 +1,5 @@
 
+import ShinyText from '../ReactBits/ShinyText/ShinyText'
 import './PageTitle.scss'
 
 
@@ -9,7 +10,16 @@ function PageTitle(props: { title: string, subTitle: string }) {
         <>
             <div className="title-component pt-20 pb-12 px-8 mt-14 m_mt-4 m_px-0">
                 <div className="container title-component__container m_p-4">
-                    <h1 className='m_mb-4'>{props.title}</h1>
+                    <ShinyText
+                        className='m_mb-4'
+                        text={props.title}
+                        speed={5}
+                        delay={0}
+                        color="#fff"
+                        shineColor="#879cd3"
+                        spread={120}
+                        direction="left"
+                    />
                     <p>{props.subTitle}</p>
                 </div>
             </div>
