9d9db36e209bdaf8e8864e70c0c59350927c112c
Pavlov-Vldmr <vpavlov95007@gmail.com>
2026-08-26 19:59:57 +0300

    feat: add fade-in effects to various components for improved user experience

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -1,5 +1,6 @@
 
 
+import FadeContent from '../ReactBits/FadeContent/FadeContent'
 import './SectionTitle.scss'
 
 
@@ -10,11 +11,19 @@ function SectionTitle(props: { title: string, text: string, plate: string, class
         <>
             <div className={`${props.className} container section-title`}>
                 <div className=" section-title pt-20 p-10 m_p-4 m_pt-10">
-                    <span className={`${props.plateColor} section-title__plate`}>{props.plate}</span>
-                    <h2 className={`${props.hColor} text_center mb-4`}>{props.title}</h2>
-                    <p className={`${props.pColor} text_center py-4`}>
-                        {props.text}
-                    </p>
+                    <FadeContent className='mb-6' blur={true} delay={.2} duration={1000} easing="ease-out" initialOpacity={0}>
+                        <span className={`${props.plateColor} section-title__plate `}>{props.plate}</span>
+                    </FadeContent>
+                    <FadeContent blur={true} delay={.4} duration={1000} easing="ease-out" initialOpacity={0}>
+                        <h2 className={`${props.hColor} text_center mb-4`}>{props.title}</h2>
+                    </FadeContent>
+                    <FadeContent blur={true} delay={.6} duration={1000} easing="ease-out" initialOpacity={0}>
+                        <p className={`${props.pColor} text_center py-4`}>
+                            {props.text}
+                        </p>
+                    </FadeContent>
+
+
                 </div>
             </div>
         </>
