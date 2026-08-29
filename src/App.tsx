a9cbca2e3254c77c7fe2bc409a6afedfcdeb7149
Pavlov-Vldmr <vpavlov95007@gmail.com>
2026-08-29 23:20:07 +0300

    hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { lazy, Suspense } from 'react'
 import CookieConsent from 'react-cookie-consent'
 import { RotatingLines } from 'react-loader-spinner'
-import { Link, Route, Routes } from 'react-router-dom'
+import { HashRouter, Link, Route, Routes } from 'react-router-dom'
 
 import { CompanyProvider } from '@/contexts/CompanyInfoContext'
 
@@ -35,41 +35,43 @@ const PageLoader = () => (
 function App() {
   return (
     <>
+      <HashRouter>
+        <CompanyProvider>
+          <ScrollToTop />
+          <Header />
 
-      <CompanyProvider>
-        <ScrollToTop />
-        <Header />
+          <div className='main'>
+            <Suspense fallback={<PageLoader />}>
+              <Routes>
+                <Route path="/infoservice-agency" element={<Home />} />
+                <Route path='/infoservice-agency/about' element={<About />} />
+                <Route path='/infoservice-agency/objects' element={<Objects />} />
+                <Route path='/infoservice-agency/services' element={<Services />} />
+                <Route path='/infoservice-agency/contacts' element={<Contacts />} />
+                <Route path='/infoservice-agency/calculator' element={<Calculator />} />
+                <Route path='/infoservice-agency/components' element={<Components />} />
+                <Route path='/infoservice-agency/*' element={<NotFound />} />
+                <Route path="/infoservice-agency/privacy-policy" element={<PrivacyPolicy />} />
+              </Routes>
+            </Suspense>
+          </div>
+          <ScrollToTopButton />
+          <CookieConsent
+            buttonText="Принять"
+            cookieName="site_privacy_cookie"
+            style={{ background: "#2B373B", color: "#FFF" }}
+            buttonStyle={{ background: "#4eaff3", color: "#FFF", fontSize: "14px" }}
+            expires={150}
+          >
+            Этот сайт использует файлы cookie. Продолжая работу, вы соглашаетесь с нашей{" "}
+            <Link to="/infoservice-agency/privacy-policy" style={{ color: "#4eaff3" }}>
+              Политикой конфиденциальности
+            </Link>.
+          </CookieConsent>
+          <Footer />
+        </CompanyProvider>
+      </HashRouter>
 
-        <div className='main'>
-          <Suspense fallback={<PageLoader />}>
-            <Routes>
-              <Route path="/infoservice-agency" element={<Home />} />
-              <Route path='/infoservice-agency/about' element={<About />} />
-              <Route path='/infoservice-agency/objects' element={<Objects />} />
-              <Route path='/infoservice-agency/services' element={<Services />} />
-              <Route path='/infoservice-agency/contacts' element={<Contacts />} />
-              <Route path='/infoservice-agency/calculator' element={<Calculator />} />
-              <Route path='/infoservice-agency/components' element={<Components />} />
-              <Route path='/infoservice-agency/*' element={<NotFound />} />
-              <Route path="/infoservice-agency/privacy-policy" element={<PrivacyPolicy />} />
-            </Routes>
-          </Suspense>
-        </div>
-        <ScrollToTopButton />
-        <CookieConsent
-          buttonText="Принять"
-          cookieName="site_privacy_cookie"
-          style={{ background: "#2B373B", color: "#FFF" }}
-          buttonStyle={{ background: "#4eaff3", color: "#FFF", fontSize: "14px" }}
-          expires={150}
-        >
-          Этот сайт использует файлы cookie. Продолжая работу, вы соглашаетесь с нашей{" "}
-          <Link to="/infoservice-agency/privacy-policy" style={{ color: "#4eaff3" }}>
-            Политикой конфиденциальности
-          </Link>.
-        </CookieConsent>
-        <Footer />
-      </CompanyProvider>
 
 
 
