10f4e5bd1b8b687c8dbfae1f3643542bdeea976e
Pavlov-Vldmr <vpavlov95007@gmail.com>
2026-08-30 22:45:06 +0300

    fix: replace AnimatedContent with ContactsInfoPlates in ContactsInfo component

--- src/components/ContactsInfo/ContactsInfo.tsx
+++ src/components/ContactsInfo/ContactsInfo.tsx
@@ -10,6 +10,7 @@ import AnimatedContent from "../ReactBits/AnimatedContent/AnimatedContent";
 import './ContactsInfo.scss'
 import BusinessHours from "../BusinessHours/BusinessHours";
 import FadeContent from "../ReactBits/FadeContent/FadeContent";
+import ContactsInfoPlates from "../ContactsInfoPlates/ContactsInfoPlates";
 
 function ContactsInfo(props: { className?: string }) {
 
@@ -25,140 +26,15 @@ function ContactsInfo(props: { className?: string }) {
                 <div id="contactsForm" className={`${props.className} contacts__block p-0`}>
                     <div className="contacts__block__items">
 
-                        <AnimatedContent
-                            distance={100}
-                            direction="vertical"
-                            reverse={false}
-                            duration={AnimContentDuraton}
-                            ease="power3.out"
-                            initialOpacity={0}
-                            animateOpacity
-                            scale={1}
-                            threshold={0.1}
-                            delay={0}
-                        >
-                            <div className='contacts__block__item'>
-                                <div className="contacts__block__item-icon-container">
-                                    <Icons.Phone className="contacts__icon icon_accent" width={20} height={20} />
-                                </div>
+                        <ContactsInfoPlates />
 
-                                <div className='item__info'>
-                                    <h4>Телефон</h4>
-                                    <span className='text_muted'><PhoneComponent className="phone" phone={`${companyInfo?.phoneMain || extraCompData.phoneMain}`} /></span>
-                                    {/* <span className="text_accent">Служба охраны 24/7</span> */}
-                                </div>
-                            </div>
-                        </AnimatedContent>
 
-                        <AnimatedContent
-                            distance={100}
-                            direction="vertical"
-                            reverse={false}
-                            duration={AnimContentDuraton}
-                            ease="power3.out"
-                            initialOpacity={0}
-                            animateOpacity
-                            scale={1}
-                            threshold={0.1}
-                            delay={0}
-                        >
-                            <div className='contacts__block__item'>
-                                <div className="contacts__block__item-icon-container">
-                                    <Icons.Map className="contacts__icon icon_accent" width={20} height={20} />
-                                </div>
 
-                                <div className='item__info'>
-                                    <h4>Адрес</h4>
-                                    <span className='text_muted'>{companyInfo?.city[cityC]?.address || extraCompData.city.yuS.address}</span>
-                                </div>
-                            </div>
-                        </AnimatedContent>
 
-                        <AnimatedContent
-                            distance={100}
-                            direction="vertical"
-                            reverse={false}
-                            duration={AnimContentDuraton}
-                            ease="power3.out"
-                            initialOpacity={0}
-                            animateOpacity
-                            scale={1}
-                            threshold={0.1}
-                            delay={0}
-                        >
-                            <div className='contacts__block__item'>
-                                <div className="contacts__block__item-icon-container">
-                                    <Icons.ShieldAlt className="contacts__icon icon_accent" width={20} height={20} />
-                                </div>
-
-                                <div className='item__info'>
-                                    <h4>{companyInfo?.personal?.phisical?.department || extraCompData.personal.phisical.department}</h4>
-                                    <span className='text_muted'>{companyInfo?.personal?.phisical?.job || extraCompData.personal.phisical.job}
-
-                                        <span className='text_black'> {companyInfo?.personal?.phisical?.name || extraCompData.personal.phisical.name}</span>
-                                    </span>
-                                    <span className='text_muted'><PhoneComponent className="phone" phone={`${companyInfo?.phoneMain || extraCompData.phoneMain}`} /> доб - <span className="text_black">{companyInfo?.personal?.phisical?.phoneExt || extraCompData.personal.phisical.phoneExt}</span> </span>
-                                    {/* <span className='text_muted'>{companyInfo?.personal?.phisical?.jobTime || extraCompData.personal.phisical.jobTime} перерыв {companyInfo?.personal?.phisical?.jobBreak || extraCompData.personal.phisical.jobBreak}</span> */}
-                                </div>
-                            </div>
-                        </AnimatedContent>
-                        <AnimatedContent
-                            distance={100}
-                            direction="vertical"
-                            reverse={false}
-                            duration={AnimContentDuraton}
-                            ease="power3.out"
-                            initialOpacity={0}
-                            animateOpacity
-                            scale={1}
-                            threshold={0.1}
-                            delay={0}
-                        >
-                            <div className='contacts__block__item'>
-                                <div className="contacts__block__item-icon-container">
-                                    <Icons.Tech className="contacts__icon icon_accent" width={20} height={20} />
-                                </div>
-
-                                <div className='item__info'>
-                                    <h4>{companyInfo?.personal?.technical?.department || extraCompData.personal.technical.department}</h4>
-                                    <span className='text_muted'>{companyInfo?.personal?.technical?.job || extraCompData.personal.technical.job}
-
-                                        <span className='text_black'> {companyInfo?.personal?.technical?.name || extraCompData.personal.technical.name}</span>
-
-                                    </span>
-                                    <span className='text_muted'><PhoneComponent className="phone" phone={`${companyInfo?.phoneMain || extraCompData.phoneMain}`} /> доб - <span className="text_black"> {companyInfo?.personal?.technical?.phoneExt || extraCompData.personal.technical.phoneExt}</span>  </span>
-                                    {/* <span className='text_muted'>{companyInfo?.personal?.technical?.jobTime || extraCompData.personal.technical.jobTime} перерыв {companyInfo?.personal?.technical?.jobBreak || extraCompData.personal.technical.jobBreak}</span> */}
-                                </div>
-                            </div>
-                        </AnimatedContent>
-                        <AnimatedContent
-                            distance={100}
-                            direction="vertical"
-                            reverse={false}
-                            duration={AnimContentDuraton}
-                            ease="power3.out"
-                            initialOpacity={0}
-                            animateOpacity
-                            scale={1}
-                            threshold={0.1}
-                            delay={0}
-                        >
-                            <div className='contacts__block__item'>
-
-                                <div className="contacts__block__item-icon-container">
-                                    <Icons.ClockAlt className="contacts__icon icon_accent" width={'100%'} height={20} />
-
-                                </div>
-                                <div className='item__info'>
-                                    <h4>Время работы</h4>
-                                    <BusinessHours />
-                                </div>
-                            </div>
-                        </AnimatedContent>
                     </div>
 
                     <FadeContent blur={true} delay={.3} duration={1000} easing="ease-out" initialOpacity={0}>
-                        <div className="contacts__block__emergency m_mb-10 bg_gradient-circle-sm">
+                        <div className="contacts__block__emergency m_mb-10 mt-10 bg_gradient-circle-sm">
                             <h4 className='mb-2'>Экстренная связь</h4>
                             <p>Для экстренных случаев и вызова группы быстрого реагирования звоните круглосуточно</p>
                             <span className="phone py-4"><PhoneComponent className='text_white' phone={`${companyInfo?.phoneEmergency || extraCompData.phoneEmergency}`} /></span>
